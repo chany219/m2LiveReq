5d04f06004d88a885008625075c94fac512ff039
Chany <chany219@naver.com>
2023-02-27 14:19:07 +0900

    re upload with edit path

---
+++ js/about.687caf92.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkm2crawl"]=self["webpackChunkm2crawl"]||[]).push([[443],{7750:function(n,t,u){u.r(t),u.d(t,{default:function(){return c}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],l=u(1001),r={},e=(0,l.Z)(r,s,a,!1,null,null,null),c=e.exports}}]);
+//# sourceMappingURL=about.687caf92.js.map
